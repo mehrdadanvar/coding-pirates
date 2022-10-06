3e25f1f4ae897ba96eac76d05684145de09f91f6
mehrdad.anvar@gmail.com <mehrdad.anvar@gmail.com>
2022-10-05 20:36:35 -0700

    inital setu up for programmin-pirates

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <Navbar />
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
+<script>
+import Navbar from "./components/Navbar.vue";
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Lora&display=swap");
+.app {
+  font-family: "Lora", serif;
+}
+</style>
